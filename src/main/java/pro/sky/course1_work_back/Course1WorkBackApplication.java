7143f487c6ccd3772d32369eb58a1584404d3535
madrahimova <darya.mad.ry@gmail.com>
2023-05-01 10:15:11 +0300

    Sample code added

---
+++ src/main/java/pro/sky/course1_work_back/Course1WorkBackApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course1_work_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course1WorkBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course1WorkBackApplication.class, args);
+    }
+
+}
